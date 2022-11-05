b5b628be16e911d7bed76b669a7dc4120b36e237
pdcourses <freshitpt@gmail.com>
2022-11-05 12:12:14 +0200

    images

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import _ from 'lodash';
-import './style.css';
+import './style.scss';
+import Icon from './png-icon.png';
 
  function component() {
    const element = document.createElement('div');
    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
    element.classList.add('red');
+
+   // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+
    return element;
  }
 
--- webpack.config.js
+++ webpack.config.js
@@ -8,14 +8,10 @@ module.exports = {
     },
     module: {
         rules: [
-          {
+          /*{
             test: /\.css$/i,
             use: ['style-loader', 'css-loader'],
-          },
-          /*{
-            test: /\.s[ac]ss$/i,
-            use: ['sass-loader'],
-          },
+          },*/
           {
             test: /\.s[ac]ss$/i,
             use: [
@@ -23,7 +19,11 @@ module.exports = {
               "css-loader",
               "sass-loader",
             ],
-          },*/
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
         ],
       },
 };
