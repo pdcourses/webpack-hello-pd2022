aa093ba540d2670e5b41e34784b93e88a51336d7
pdcourses <freshitpt@gmail.com>
2022-11-05 11:46:10 +0200

    style loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,27 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          /*{
+            test: /\.s[ac]ss$/i,
+            use: ['sass-loader'],
+          },
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              "style-loader",
+              "css-loader",
+              "sass-loader",
+            ],
+          },*/
+        ],
+      },
 };
